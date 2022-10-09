1a4647facc939fc6e61a6561d099d0df201df95c
Dmitrii <dmasny@yandex.ru>
2022-10-10 01:03:22 +0500

    07 done, fix pylint

--- 07/ReaderWriter.py
+++ 07/ReaderWriter.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import json
 import csv
+import io
 # насколько я понял, нужно было сделать абстрактный базовый класс
 # а потом в наследниках переопределить методы, но этого вроде не было на 7 лекции
 # поэтому я опирался на этот источник https://docs-python.ru/tutorial/klassy-jazyke-python/abstraktnye-klassy/
@@ -20,43 +21,46 @@ class TxtReader(BaseReader):
 
 class TxtWriter(BaseWriter):
     def dump(self, fileobj, data):
-        fileobj.writelines(data)
+        if isinstance(data, list):
+            fileobj.write('\n'.join(data))
+        else:
+            fileobj.write(data)
 
 class CsvReader(BaseReader):
     def read(self, fileobj):
         data = []
-        csvreader = csv.reader(fileobj)
+        csvreader = csv.reader(fileobj, delimiter = ',')
         for row in csvreader:
-            data.append(','.join([elem for elem in row]))
+            data.append(','.join(row))
         return data
     
 class CsvWriter(BaseWriter):
     def dump(self, fileobj, data):
-            csvwriter = csv.writer(fileobj, delimiter = ',')
-            csvwriter.writerow(data)
+        csvwriter = csv.writer(fileobj)
+        csvwriter.writerow(data)
 
 class JsonReader(BaseReader):
     def read(self, fileobj):
-        return json.load(fileobj)
+        return json.loads(fileobj)
 
 class JsonWriter(BaseWriter):
     def dump(self, fileobj, data):
         json.dump(data, fileobj)
 
 def read_data(fileobj, reader: BaseReader):
-    with open(fileobj, 'r') as f:
-        return reader.read(f)
+    return reader.read(fileobj)
 
 def dump_data(data, fileobj, writer: BaseWriter):
-    with open(fileobj, 'w') as f:
-        writer.dump(f, data)
+    writer.dump(fileobj, data)
 
 if __name__ == '__main__':
-    print(read_data('tt.txt', reader = TxtReader()))
-    dump_data(['hello', '\nhow are', '\n u'], 'test_file.txt', writer = TxtWriter())
-    print(read_data('tt2.csv', reader = CsvReader()))
-    dump_data(['1,2,3,4', '5,6,7,8'], 'test_file_csv.csv', writer = CsvWriter())
-    print(read_data('test_json.json', reader = JsonReader()))
-    dump_data({"x": "1"}, 'write_json.json', writer=JsonWriter())
-
-
+    buf = io.StringIO()
+    dump_data(['word1', 'word2', 'word3'], buf, CsvWriter())
+    print(buf.getvalue())
+    buf.seek(0)
+    print(read_data(buf, CsvReader()))
+    dump_data(['word4', 'word5', 'word6'], buf, CsvWriter())
+    print(buf.getvalue())
+    buf.seek(0)
+    print(read_data(buf, CsvReader()))
+    
--- 07/reader_writer_tests.py
+++ 07/reader_writer_tests.py
@@ -1,6 +1,65 @@
 import io
 from ReaderWriter import * # на паре говорили, что так лучше не делать, но и километровую строку писать тоже не хочется
 import unittest
-
+import json
 class ReadwrWriterTest(unittest.TestCase):
-    pass
+    # проверка на запись json
+    def test_json_write(self):
+        buf = io.StringIO()
+        self.assertEqual(buf.getvalue(), '')
+        dump_data({'name' : 'Jane', 'age' : 23, 'occupation' : 'teacher'}, buf, JsonWriter())
+        self.assertEqual(buf.getvalue(), json.dumps({'name' : 'Jane', 'age' : 23, 'occupation' : 'teacher'}))
+
+    # проверка на чтение json
+    def test_json_read(self):
+        buf = io.StringIO()
+        self.assertEqual(buf.getvalue(), '')
+        dump_data({'name' : 'Jane', 'age' : 23, 'occupation' : 'teacher'}, buf, JsonWriter())
+        self.assertEqual(read_data(buf.getvalue(), JsonReader()), {'name' : 'Jane', 'age' : 23, 'occupation' : 'teacher'})
+    
+    # проверка на запись txt
+    def test_txt_write(self):
+        buf = io.StringIO()
+        self.assertEqual(buf.getvalue(), '')
+        dump_data(['abc', 'sdfsf', 'sfsf'], buf, TxtWriter())
+        self.assertEqual(buf.getvalue(), 'abc\nsdfsf\nsfsf')
+
+        buf = io.StringIO()
+        dump_data('abc', buf, TxtWriter())
+        self.assertEqual(buf.getvalue(), 'abc')
+
+    # проверка на чтение txt
+    def test_txt_read(self):
+        buf = io.StringIO()
+        self.assertEqual(buf.getvalue(), '')
+        dump_data(['abc', 'sdfsf', 'sfsf'], buf, TxtWriter())
+        buf.seek(0)
+        self.assertEqual(read_data(buf, TxtReader()), ['abc', 'sdfsf', 'sfsf'])
+
+        buf = io.StringIO()
+        self.assertEqual(buf.getvalue(), '')
+        dump_data('abc', buf, TxtWriter())
+        buf.seek(0)
+        self.assertEqual(read_data(buf, TxtReader()), ['abc'])
+
+    # проверка на запись csv
+    def test_csv_write(self):
+        buf = io.StringIO()
+        self.assertEqual(buf.getvalue(), '')
+        dump_data(['word1', 'word2', 'word3'], buf, CsvWriter())
+        print(buf.getvalue())
+        self.assertEqual(buf.getvalue(), 'word1,word2,word3\r\n')
+    # проверка на чтение csv
+    def test_csv_read(self):
+        buf = io.StringIO()
+        self.assertEqual(buf.getvalue(), '')
+        dump_data(['word1', 'word2', 'word3'], buf, CsvWriter())
+        buf.seek(0)
+        self.assertEqual(read_data(buf, CsvReader()), ['word1,word2,word3'])
+
+        dump_data(['word4', 'word5', 'word6'], buf, CsvWriter())
+        buf.seek(0)
+        self.assertEqual(read_data(buf, CsvReader()), ['word1,word2,word3', 'word4,word5,word6'])
+        
+if __name__ == '__main__':
+    unittest.main()
