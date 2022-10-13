8cb2270a0713192f83ddd692b5560335b52e3bb3
Dmitrii <dmasny@yandex.ru>
2022-10-13 19:30:42 +0300

    08 is added

---
+++ 08/find_anagram.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+def find_anagrams(text: str, pattern: str):
+    res = []
+    if pattern == '':
+        return res
+    if len(pattern) > len(text):
+        return res
+    pattern_dict = defaultdict(int)
+    for el in pattern:
+        pattern_dict[el] += 1
+    l_ptr = 0
+    while l_ptr <= len(text) - len(pattern):
+        if l_ptr == 0:
+            cur_dict = defaultdict(int)
+            for i in range(len(pattern)):
+                cur_dict[text[i]] += 1
+        else:
+            cur_dict[text[l_ptr + len(pattern) - 1]] += 1
+
+        if cur_dict == pattern_dict:
+            res.append(l_ptr)
+
+        cur_dict[text[l_ptr]] -= 1
+        if cur_dict[text[l_ptr]] == 0:
+            del cur_dict[text[l_ptr]]
+        l_ptr += 1
+
+    return res
+
+if __name__ == '__main__':
+    print(find_anagrams('abab', 'ab'))
+    
---
+++ 08/tests_find_anagram.py
@@ -0,0 +1,38 @@
+from find_anagram import find_anagrams
+import unittest
+
+class TestFinder(unittest.TestCase):
+
+    def test_finder(self):
+        pattern = 'a'
+        text = 'aaaaaa'
+        self.assertEqual(find_anagrams(text, pattern), [0, 1, 2, 3, 4, 5])
+
+        pattern = 'ab'
+        text = 'abab'
+        self.assertEqual(find_anagrams(text, pattern), [0, 1, 2])
+
+        pattern = ''
+        text = 'abab'
+        self.assertEqual(find_anagrams(text, pattern), [])
+
+        pattern = 'abccccc'
+        text = 'abab'
+        self.assertEqual(find_anagrams(text, pattern), [])
+
+        pattern = 'ab'
+        text = 'abcbdecbdeafsfggefgsbc'
+        self.assertEqual(find_anagrams(text, pattern), [0])
+
+        pattern = 'bd'
+        text = 'abcbdecbdeafsfggefgsbc'
+        self.assertEqual(find_anagrams(text, pattern), [3, 7])
+
+        pattern = 'fc'
+        text = 'fcffccffcccfccf'
+        self.assertEqual(find_anagrams(text, pattern), [0, 1, 3, 5, 7, 10, 11, 13])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
