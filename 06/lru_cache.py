929de7e8c986342c5e3479bb48873b53230bf9d9
Dmitrii <dmasny@yandex.ru>
2022-10-17 13:14:12 +0300

    06 rewritten

--- 06/lru_cache.py
+++ 06/lru_cache.py
@@ -2,22 +2,24 @@ class LRUCache:
 
     def __init__(self, limit = 42):
         self.capacity = limit
+        self.operation_counter = 0
         self.data_dict = {}
-        self.keys = []
+        self.order_dict = {}
         
     def get(self, key):
-        if key not in self.keys:
+        if key not in self.data_dict:
             return -1
         elem = self.data_dict[key]
-        self.keys.remove(key)
-        self.keys.append(key)
+        self.operation_counter += 1
+        self.order_dict[key] = self.operation_counter
         return elem
         
     def set(self, key, value):
-        if key not in self.keys and len(self.data_dict.keys()) == self.capacity:
-            key_to_delete = self.keys.pop(0)
+        if key not in self.data_dict and len(self.data_dict.keys()) == self.capacity:
+            key_to_delete = min(self.order_dict, key = self.order_dict.get)
             del self.data_dict[key_to_delete]
-        if key in self.keys:
-            self.keys.remove(key)
+            del self.order_dict[key_to_delete]
         self.data_dict[key] = value
-        self.keys.append(key)
+        self.operation_counter += 1
+        self.order_dict[key] = self.operation_counter
+
