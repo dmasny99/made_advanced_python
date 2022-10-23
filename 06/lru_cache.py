df5c64ee8e6c54bea0ea25b7ece71597cb7b6d16
Dmitrii <dmasny@yandex.ru>
2022-10-23 12:27:49 +0300

    06 fixed

--- 06/lru_cache.py
+++ 06/lru_cache.py
@@ -5,21 +5,21 @@ class LRUCache:
         self.operation_counter = 0
         self.data_dict = {}
         self.order_dict = {}
-        
+
     def get(self, key):
         if key not in self.data_dict:
-            return -1
+            return
         elem = self.data_dict[key]
         self.operation_counter += 1
         self.order_dict[key] = self.operation_counter
         return elem
-        
+
     def set(self, key, value):
-        if key not in self.data_dict and len(self.data_dict.keys()) == self.capacity:
+        if key not in self.data_dict and \
+                len(self.data_dict.keys()) == self.capacity:
             key_to_delete = min(self.order_dict, key = self.order_dict.get)
             del self.data_dict[key_to_delete]
             del self.order_dict[key_to_delete]
         self.data_dict[key] = value
         self.operation_counter += 1
         self.order_dict[key] = self.operation_counter
-
--- 06/tests_lru.py
+++ 06/tests_lru.py
@@ -8,37 +8,89 @@ class LRUTests(unittest.TestCase):
         lru = LRUCache(2)
         self.assertEqual(lru.data_dict, {})
         lru.set(1, 1)
-        self.assertEqual(lru.data_dict, {1: 1})
-        lru.set(2, 2)
-        self.assertEqual(lru.data_dict, {1: 1, 2: 2})
-        lru.set(3, 3)
-        self.assertEqual(lru.data_dict, {3: 3, 2: 2})
-        
-    def test_get(self):
-        lru = LRUCache(2)
-        self.assertEqual(lru.get(0), -1)
-        lru.set(1, 1)
+        self.assertEqual(lru.get(1), 1)
         lru.set(2, 2)
         self.assertEqual(lru.get(1), 1)
         self.assertEqual(lru.get(2), 2)
+        lru.set(3, 3)
+        self.assertEqual(lru.get(2), 2)
+        self.assertEqual(lru.get(3), 3)
+        self.set(2,2)
 
-    def test_replace_cases(self):
+    def test_from_task(self):
         lru = LRUCache(2)
-        self.assertEqual(lru.data_dict, {})
-        lru.set(1, 1)
-        self.assertEqual(lru.data_dict, {1: 1})
-        lru.set(2, 2)
-        self.assertEqual(lru.data_dict, {1: 1, 2: 2})
-        self.assertEqual(lru.get(1), 1)
-        lru.set(5, 5)
-        self.assertEqual(lru.data_dict, {1: 1, 5: 5})
-        lru.set(6, 6)
-        self.assertEqual(lru.get(5), 5)
-        lru.set(7, 7)
-        self.assertEqual(lru.get(5), 5)
-        lru.set(9, 9)
-        self.assertEqual(lru.data_dict, {5: 5, 9: 9})
+        lru.set('k1', 'val1')
+        lru.set('k2', 'val2')
+        self.assertIsNone(lru.get('k3'))
+        self.assertEqual(lru.get('k2'), 'val2')
+        self.assertEqual(lru.get('k1'), 'val1')
 
+        lru.set('k3', 'val3')
+        print(lru.get('k3'))
+        self.assertEqual(lru.get('k3'), 'val3')
+        self.assertIsNone(lru.get('k2'))
+        self.assertEqual(lru.get('k1'), 'val1')
+
+    def test_capacity_one(self):
+        lru = LRUCache(1)
+        lru.set('k1', 'val1')
+        self.assertEqual(lru.get('k1'), 'val1')
+        lru.set('k2', 'val2')
+        self.assertEqual(lru.get('k2'), 'val2')
+        lru.set('k3', 'val3')
+        lru.set('k4', 'val4')
+        self.assertEqual(lru.get('k4'), 'val4')
+        self.assertIsNone(lru.get('k3'))
+
+    def test_full_replacement(self):
+        lru = LRUCache(3)
+        lru.set('k1', 'val1')
+        lru.set('k2', 'val2')
+        lru.set('k3', 'val3')
+
+        self.assertEqual(lru.get('k1'), 'val1')
+        self.assertEqual(lru.get('k2'), 'val2')
+        self.assertEqual(lru.get('k3'), 'val3')
+
+        lru.set('k4', 'val4')
+        lru.set('k5', 'val5')
+        lru.set('k6', 'val6')
+
+        self.assertEqual(lru.get('k4'), 'val4')
+        self.assertEqual(lru.get('k5'), 'val5')
+        self.assertEqual(lru.get('k6'), 'val6')
+
+        self.assertIsNone(lru.get('k1'))
+        self.assertIsNone(lru.get('k2'))
+        self.assertIsNone(lru.get('k3'))
+
+    def test_rewrite_key(self):
+        lru = LRUCache(3)
+        lru.set('k1', 'val1')
+        lru.set('k2', 'val2')
+        lru.set('k3', 'val3')
+
+        self.assertEqual(lru.get('k1'), 'val1')
+        self.assertEqual(lru.get('k2'), 'val2')
+        self.assertEqual(lru.get('k3'), 'val3')
+
+        lru.set('k1', 'val1_new')
+        self.assertEqual(lru.get('k1'), 'val1_new')
+
+        lru.set('k4', 'val4')
+        self.assertEqual(lru.get('k1'), 'val1_new')
+        self.assertIsNone(lru.get('k2'))
+        self.assertEqual(lru.get('k3'), 'val3')
+        self.assertEqual(lru.get('k4'), 'val4')
+
+        lru.set('k1', 'val1_new_new')
+        self.assertEqual(lru.get('k1'), 'val1_new_new')
+
+        lru.set('k5', 'val5')
+        self.assertEqual(lru.get('k1'), 'val1_new_new')
+        self.assertIsNone(lru.get('k3'))
+        self.assertEqual(lru.get('k4'), 'val4')
+        self.assertEqual(lru.get('k5'), 'val5')
 
 if __name__ == '__main__':
     unittest.main()
